f0e1e8ff9675e4a28aad3c406f994ceb70f54d0d
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2015-10-05 15:41:03 -0500

    * Fixed gulp pipeline to ensure metrics builds at the same time as the core

--- dist/okanjo-metrics.js
+++ dist/okanjo-metrics.js
@@ -1,4 +1,4 @@
-/*! okanjo-metrics.js v0.6.1 | (c) 2013 Okanjo Partners Inc | https://okanjo.com/ */
+/*! okanjo-metrics.js v0.6.3 | (c) 2013 Okanjo Partners Inc | https://okanjo.com/ */
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
@@ -559,7 +559,7 @@
     var config = okanjo.config = okanjo.config || {};
 
     // okanjo-ads api key
-    config.key = undefined;
+    config.key = config.key || undefined;
 
         // Marketplace config
     config.marketplace = {
