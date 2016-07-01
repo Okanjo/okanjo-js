13df6f0d277739f5ce8a13456d2e1369174fb9b8
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2016-07-01 10:43:39 -0500

    Tagged as v0.7.1

--- dist/okanjo-metrics.js
+++ dist/okanjo-metrics.js
@@ -1,4 +1,4 @@
-/*! okanjo-metrics.js v0.7.0 | (c) 2013 Okanjo Partners Inc | https://okanjo.com/ */
+/*! okanjo-metrics.js v0.7.1 | (c) 2013 Okanjo Partners Inc | https://okanjo.com/ */
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
