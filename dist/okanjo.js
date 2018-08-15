3d29f71869bae6ae8245a13909eac4bbaca25d09
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2018-08-15 15:23:16 -0500

    Tagged as v1.8.1

--- dist/okanjo.js
+++ dist/okanjo.js
@@ -1,4 +1,4 @@
-/*! okanjo-js v1.8.0 | (c) 2013 Okanjo Partners Inc | https://okanjo.com/ */
+/*! okanjo-js v1.8.1 | (c) 2013 Okanjo Partners Inc | https://okanjo.com/ */
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
@@ -323,7 +323,7 @@ var okanjo = function (window, document) {
         /**
          * Okanjo version
          */
-        version: "1.8.0",
+        version: "1.8.1",
 
         /**
          * Placeholder
